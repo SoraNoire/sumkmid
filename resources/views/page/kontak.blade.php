3c103af8a6ca00116ab9475a7bdb412de30db301
bagas dicko <fahmial51@gmail.com>
2017-11-28 17:21:34 +0700

    add var fontColor

--- resources/views/page/kontak.blade.php
+++ resources/views/page/kontak.blade.php
@@ -2,6 +2,39 @@
 
 @section('content')
 
+<div class="breadcrumb">
+	<div class="container">
+		<h2>Mentor</h2>
+	</div>
+</div>
 
+<div id="contact">
+	<div id="map"></div>
+	<div class="container">
+		<div class="the-row">
+			<div class="col-6 contact-address">
+				<h3>Head Office</h3>
+				<p style="margin: 0">Jln. Kebon Kacang Raya No. 25 Tanah Abang</p>
+				<p style="margin: 0">Jakarta Pusat, Indonesia</p>
+				<p style="margin-bottom: 14px">Kode Pos 10240</p>
+				<p style="margin-bottom: 8px"><i class="fa fa-phone" aria-hidden="true"></i> (021) 3917399</p>
+				<p><i class="fa fa-envelope" aria-hidden="true"></i> info@mdirect.id</p>
+			</div>
+			<div class="col-6 contact-form">
+				<h3>Leave a message</h3>
+				<form action="" method="post">
+					<input type="text" name="nama" placeholder="nama">
+					<input type="email" name="email" placeholder="email">
+					<textarea name="pesan" placeholder="pesan"></textarea>
+					<button type="sumbit" class="button blue">Kirim</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>function initMap(){var e=-6.193724,n=106.817485,o={lat:e,lng:n},a=new google.maps.Map(document.getElementById("map"),{zoom:16,center:o});new google.maps.Marker({position:o,map:a})}</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAyE7KLJT2rPz-8N2Fi6Ump53yabrvNj5g&callback=initMap" async defer></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
 
 @endsection
