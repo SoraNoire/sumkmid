1a157e79b79b0e9cfdecaf2493028bcc1eea5d07
bagas dicko <fahmial51@gmail.com>
2017-12-08 13:40:08 +0700

    input multiple htm & mentor bisa nama bebas

--- resources/views/modules/event/admin/add_event.blade.php
+++ resources/views/modules/event/admin/add_event.blade.php
@@ -75,25 +75,51 @@
                                 <div class="form-group">
                                     <small>Free</small> 
                                     <label class="switch">
-                                      <input type="checkbox" name="htm_free" value="1" {{-- $hta_check --}}>
+                                      <input type="checkbox" name="htm_free" value="free" {{ old('htm_free') }}>
                                       <span class="slider round"></span>
                                     </label>
                                 </div>
-                                <div class="input-group">
-                                    <span class="input-group-addon">Rp</span>
-                                    <input value="{{ old('htm') }}" class="form-control" type="text" name="htm">
+                                <div id="htm-parent">
+                                    <div class="row" id="htm-1" data-id="1">
+                                        <div class="form-group col-sm-6">
+                                            <label>Nominal</label>
+                                            <div class="input-group">
+                                                <span class="input-group-addon">Rp</span>
+                                                <input value="{{ old('htm_nominal') }}" class="form-control" type="text" name="htm_nominal[]">
+                                            </div>
+                                        </div>
+                                        <div class="form-group col-sm-6">
+                                            <label>Label</label>
+                                            <div class="input-group">
+                                                <input value="{{ old('htm_label') }}" type="text" name="htm_label[]" class="form-control">
+                                                <span class="input-group-btn">
+                                                    <button class="btn btn-info" class="add-htm" onclick="add_htm()" type="button">+</button>
+                                                </span>
+                                                <span class="input-group-btn">
+                                                    <button class="btn btn-warning" class="remove-htm" onclick="remove_htm('htm-1')" type="button">-</button>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div class="form-group event-type-online">
                                 <label>URL</label>
                                 <input class="form-control" type="url" name="event_url" value="{{ old('event_url') }}">
                             </div>
+
                             <div class="form-group">
                                <label class="control-label">Select Mentor</label>
-                                <select name="mentor[]" class="form-control myselect2" multiple>
+                                <select name="mentor_registered[]" class="form-control myselect2" multiple>
                                    @foreach ($mentors as $mentor)
                                         <option value="{{ $mentor->id }}" {{ old('mentor') == $mentor->id ? 'selected' : '' }}>{{ $mentor->name }}</option>
                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                               <label class="control-label">Input mentor that <span style="color: #d9534f;">not</span> registered on MDirect</label>
+                                <select name="mentor_not_registered[]" class="form-control mytag" multiple>
+                                   
                                 </select>
                             </div>
 
@@ -123,10 +149,17 @@
                                         <input class="form-control" size="16" type="text" value="{{ old('closed_date') }}" name="closed_date" readonly required>
                                         <span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>
                                     </div>
-                                    <input type="number" name="hour_close" id="hour_open" min="0" max="23" maxlength="2" value="{{ old('hour_close') }}" placeholder="HH" class="form-control">&nbsp;:
-                                    <input type="number" name="minute_close" id="minute_open" min="0" max="59" maxlength="2" value="{{ old('minute_close') }}" placeholder="mm" class="form-control">
+                                    <input type="number" name="hour_close" id="hour_open" min="0" max="23" maxlength="2" value="{{ old('hour_close') }}" placeholder="HH" class="form-control" required>&nbsp;:
+                                    <input type="number" name="minute_close" id="minute_open" min="0" max="59" maxlength="2" value="{{ old('minute_close') }}" placeholder="mm" class="form-control" required>
                                     <label>WIB</label>
                                 </div>
+                                @if ($errors->has('closed_date'))
+                                <div class="has-error">
+                                    <span class="help-block">
+                                        <strong>This field is required</strong>
+                                    </span>
+                                </div>
+                                @endif
                             </div>
 
                         </div>
@@ -186,24 +219,6 @@
                     </div>
                 </div>
 
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                          Featured Image <a data-toggle="collapse" href="#event-fimg"><i style="float: right;" class="fa fa-caret-down" aria-hidden="true"></i></a>
-                        </h4>
-                    </div>
-                    <div id="event-fimg" class="panel-collapse collapse in">
-                        <div class="panel-body form-group">
-                            <a id="browse_fimg_post" data-toggle="modal" data-target="#myFimg" class="btn btn-round btn-fill btn-default" style="margin-bottom: 10px;">Set Featured Image</a>
-                            <input type="hidden" name="featured_image" id="featured_img" value="{{ old('featured_image') }}">
-                            <div class="preview-fimg-wrap" style="display: {{ old('featured_image') != '' ? 'block' : ''  }};">
-                                <div class="preview-fimg" style="background-image: url({{ old('featured_image') }});"></div>
-                                <a href="#" onclick="remove_fimg()" class="remove-fimg"><i class="fa fa-times" aria-hidden="true"></i> Remove Featured Image</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
 
@@ -237,28 +252,6 @@
             </thead>
         </table>
     </div>
-    </div>
-</div>
-
-<div class="custom-modal fimg-modal">
-<div class="close-modal" id="close_fimg_post" data-toggle="modal" data-target="#myFimg">X</div>
-    <div class="card">
-        <div class="btn btn-round btn-fill btn-info" style="margin-bottom: 10px;" onclick="document.getElementById('uploadfimg').click();">Upload media +
-            <form id="actuploadfimg" method="post" action="{{ URL::to('/administrator/act_new_media') }}" accept-charset="UTF-8" enctype="multipart/form-data">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="file" id="uploadfimg" name="media[]" style="cursor: pointer;display: none;" multiple>
-            </form>
-        </div>
-        <div class="card-content table-responsive">
-            <table style="width: 100%;" class="table mediatable" id="FeaturedImg">
-                <thead >
-                    <th>Preview</th>
-                    <th>Judul</th>
-                    <th>Tanggal</th>
-                    <th>Aksi</th>
-                </thead>
-            </table>
-        </div>
     </div>
 </div>
 @endsection
--- resources/views/modules/event/admin/edit_event.blade.php
+++ resources/views/modules/event/admin/edit_event.blade.php
@@ -82,17 +82,42 @@
                                 <div class="form-group">
                                     <small>Free</small> 
                                     <label class="switch">
-                                      <input type="checkbox" name="htm_free" value="1" {{-- $hta_check --}}>
+                                      <input type="checkbox" name="htm_free" value="free" {{ $htm == 'free' ? 'checked' : '' }}>
                                       <span class="slider round"></span>
                                     </label>
                                 </div>
                                 <div id="htm-parent">
+                                    @if ( is_array($htm) && count($htm) > 0 )
+                                        @foreach ($htm as $htm)
+                                        <div class="row" id="htm-1" data-id="1">
+                                            <div class="form-group col-sm-6">
+                                                <label>Nominal</label>
+                                                <div class="input-group">
+                                                    <span class="input-group-addon">Rp</span>
+                                                    <input value="{{ $htm->nominal }}" class="form-control" type="text" name="htm_nominal[]">
+                                                </div>
+                                            </div>
+                                            <div class="form-group col-sm-6">
+                                                <label>Label</label>
+                                                <div class="input-group">
+                                                    <input value="{{ $htm->label }}" type="text" name="htm_label[]" class="form-control">
+                                                    <span class="input-group-btn">
+                                                        <button class="btn btn-info" class="add-htm" onclick="add_htm()" type="button">+</button>
+                                                    </span>
+                                                    <span class="input-group-btn">
+                                                        <button class="btn btn-warning" class="remove-htm" onclick="remove_htm('htm-1')" type="button">-</button>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                    @else
                                     <div class="row" id="htm-1" data-id="1">
                                         <div class="form-group col-sm-6">
                                             <label>Nominal</label>
                                             <div class="input-group">
                                                 <span class="input-group-addon">Rp</span>
-                                                <input value="{{ $htm }}" class="form-control" type="text" name="htm[]">
+                                                <input value="" class="form-control" type="text" name="htm_nominal[]">
                                             </div>
                                         </div>
                                         <div class="form-group col-sm-6">
@@ -108,6 +133,7 @@
                                             </div>
                                         </div>
                                     </div>
+                                    @endif
                                 </div>
                             </div>
                             <div class="form-group event-type-online">
@@ -115,11 +141,19 @@
                                 <input class="form-control" type="url" name="event_url" value="{{ old('event_url') }}">
                             </div>
                             <div class="form-group">
-                               <label class="control-label">Select Mentor</label>
-                                <select name="mentor[]" class="form-control myselect2" multiple>
+                               <label class="control-label">Select mentor that registered on MDirect</label>
+                                <select name="mentor_registered[]" class="form-control myselect2" multiple>
                                    @foreach ($mentors as $mentor)
-                                        <option value="{{ $mentor->id }}" {{ is_array($selected_mentors) && in_array($mentor->id, $selected_mentors) ? 'selected' : ''}}>{{ $mentor->name }}</option>
+                                        <option value="{{ $mentor->id }}" {{ is_array($mentor_registered) && in_array($mentor->id, $mentor_registered) ? 'selected' : ''}}>{{ $mentor->name }}</option>
                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                               <label class="control-label">Input mentor that <span style="color: #d9534f;">not</span> registered on MDirect</label>
+                                <select name="mentor_not_registered[]" class="form-control mytag" multiple>
+                                    @foreach ($mentor_not_registered as $mentor_n)
+                                    <option value="{{ $mentor_n }}" selected="selected">{{ $mentor_n }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                             <div class="form-group">
@@ -148,10 +182,17 @@
                                         <input class="form-control" size="16" type="text" value="{{ $closed_date }}" name="closed_date" readonly required>
                                         <span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>
                                     </div>
-                                    <input type="number" name="hour_close" id="hour_open" min="0" max="23" maxlength="2" value="{{ $hour_close }}" placeholder="HH" class="form-control event_time">&nbsp;:
-                                    <input type="number" name="minute_close" id="minute_open" min="0" max="59" maxlength="2" value="{{ $minute_close }}" placeholder="mm" class="form-control event_time">
+                                    <input type="number" name="hour_close" id="hour_open" min="0" max="23" maxlength="2" value="{{ $hour_close }}" placeholder="HH" class="form-control event_time" required>&nbsp;:
+                                    <input type="number" name="minute_close" id="minute_open" min="0" max="59" maxlength="2" value="{{ $minute_close }}" placeholder="mm" class="form-control event_time" required>
                                     <label>WIB</label>
                                 </div>
+                                @if ($errors->has('closed_date'))
+                                <div class="has-error">
+                                    <span class="help-block">
+                                        <strong>This field is required</strong>
+                                    </span>
+                                </div>
+                                @endif
                             </div>
 
                         </div>
@@ -211,24 +252,6 @@
                     </div>
                 </div>
 
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4 class="panel-title">
-                          Featured Image <a data-toggle="collapse" href="#event-fimg"><i style="float: right;" class="fa fa-caret-down" aria-hidden="true"></i></a>
-                        </h4>
-                    </div>
-                    <div id="event-fimg" class="panel-collapse collapse in">
-                        <div class="panel-body form-group">
-                            <a id="browse_fimg_post" data-toggle="modal" data-target="#myFimg" class="btn btn-round btn-fill btn-default" style="margin-bottom: 10px;">Set Featured Image</a>
-                            <input type="hidden" name="featured_image" id="featured_img" value="{{ $featured_image }}">
-                            <div class="preview-fimg-wrap" style="display: {{ $featured_image != '' ? 'block' : ''  }};">
-                                <div class="preview-fimg" style="background-image: url({{ $featured_image }});"></div>
-                                <a href="#" onclick="remove_fimg()" class="remove-fimg"><i class="fa fa-times" aria-hidden="true"></i> Remove Featured Image</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
 
@@ -261,28 +284,6 @@
             </thead>
         </table>
     </div>
-    </div>
-</div>
-
-<div class="custom-modal fimg-modal">
-<div class="close-modal" id="close_fimg_post" data-toggle="modal" data-target="#myFimg">X</div>
-    <div class="card">
-        <div class="btn btn-round btn-fill btn-info" style="margin-bottom: 10px;" onclick="document.getElementById('uploadfimg').click();">Upload media +
-            <form id="actuploadfimg" method="post" action="{{ URL::to('/administrator/act_new_media') }}" accept-charset="UTF-8" enctype="multipart/form-data">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="file" id="uploadfimg" name="media[]" style="cursor: pointer;display: none;" multiple>
-            </form>
-        </div>
-        <div class="card-content table-responsive">
-            <table style="width: 100%;" class="table mediatable" id="FeaturedImg">
-                <thead >
-                    <th>Preview</th>
-                    <th>Judul</th>
-                    <th>Tanggal</th>
-                    <th>Aksi</th>
-                </thead>
-            </table>
-        </div>
     </div>
 </div>
 @endsection
