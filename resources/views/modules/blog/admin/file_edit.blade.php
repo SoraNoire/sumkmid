00e69d8566f86b8907a384ce78192cefb389da9d
bagas dicko <fahmial51@gmail.com>
2017-12-21 18:25:11 +0700

    vendor publish

---
+++ resources/views/modules/blog/admin/file_edit.blade.php
@@ -0,0 +1,20 @@
+@extends('blog::layouts.master')
+
+@section('content')
+<div class="col-md-12">
+<div class="card">
+    <div class="card-header" data-background-color="green">
+        <h4 class="title">Edit File</h4>
+    </div>
+
+    <form method="post" action="{{ route('panel.file__update',$id) }}" accept-charset="UTF-8">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="form-group">
+            <label class="control-label">Label</label>
+            <input class="form-control" type="text" name="label" value="{{ $label }}">
+        </div>
+        <button type="submit" class="btn btn-success pull-right">Save</button>
+    </form>
+</div>
+</div>
+@stop
---
+++ resources/views/modules/event/admin/index.blade.php
@@ -0,0 +1,30 @@
+@extends('blog::layouts.master')
+
+@section('content')
+<div class="col-md-12">
+<form method="post" action="{{ route('panel.event__delete__mass') }}" accept-charset="UTF-8">
+<a href="{{ route('panel.event__add') }}" class="btn btn-round btn-fill btn-info">New Event +<div class="ripple-container"></div></a>
+
+<input type="hidden" name="_token" value="{{ csrf_token() }}">
+<input type="hidden" name="id" class="bulk-delete-id">
+<button type="sumbit" style="display: none;" class="btn btn-round btn-fill btn-danger bulk-delete-item">Delete <span class="bulk-delete-count"></span> Event</button>
+</form>
+<div class="card">
+    <div class="card-header" data-background-color="green">
+        <h4 class="title">Events</h4>
+        <p class="category">All Event</p>
+    </div>
+
+    <div class="card-content table-responsive">
+        <table class="table mydatatable" id="event-table">
+            <thead>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Published At</th>
+                <th>Action</th>
+            </thead>
+        </table>
+    </div>
+</div>
+</div>
+@stop
