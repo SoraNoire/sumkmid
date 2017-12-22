008e1de09a442aa9eb9b15f0812058743b524781
Rian <damanik183@gmail.com>
2017-12-22 14:05:45 +0700

    small

--- resources/views/layouts/publicbase.blade.php
+++ resources/views/layouts/publicbase.blade.php
@@ -98,7 +98,7 @@
                                         <span id="profileTrigger" class="icon i-cog"></span>
                                     </div>
                                     <ul>
-                                        @if('admin' != app()->OAuth->Auth()->role)
+                                        @if('admin' == app()->OAuth->Auth()->role)
                                         <li><a href="{{route('panel.dashboard')}}">Dashboard</a></li>
                                         @endif
                                         <li><a href="{{route('user_setting')}}">Edit Profile</a></li>
