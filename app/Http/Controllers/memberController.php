f5b47c65372d8030642a15a3e3208107bd455e96
Rian <damanik183@gmail.com>
2018-01-12 14:14:47 +0700

    update profil pic

--- app/Http/Controllers/memberController.php
+++ app/Http/Controllers/memberController.php
@@ -103,14 +103,16 @@ class memberController extends Controller
 			'email' => $user->data->email,
 			'avatar'=> $path
 		];
-
+        $user = app()->OAuth->Auth(app()->OAuth->Auth()->token);
         if($user && $user->success){
             $user = $user->data;            
         }else{
             $user = app()->OAuth->auth();
         }
 		if(app()->OAuth::meUpdate($data)){
-            Users::where('id',app()->OAuth::Auth()->id)->update(['avatar'=>$user->foto_profil]);
+            if($user->foto_profil){
+                Users::where('id',app()->OAuth::Auth()->id)->update(['avatar'=>$user->foto_profil]);
+            }
 			return $user->foto_profil;
 		}else{
 			return 'fail';
