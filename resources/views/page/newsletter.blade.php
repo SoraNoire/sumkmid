dceb7a00efc6b94a5c46d96185e83ef1093c9f7d
Rian <damanik183@gmail.com>
2018-01-16 11:20:35 +0700

    mce-form action

--- resources/views/page/newsletter.blade.php
+++ resources/views/page/newsletter.blade.php
@@ -12,7 +12,7 @@
 	<div class="container">
 		<!-- Begin MailChimp Signup Form -->
 		<div id="mc_embed_signup">
-			<form action="https://sahabatumkm.us17.list-manage.com/subscribe/post?u=b4446f3e9bb846772656226e8&amp;id=ddb0a75035" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
+			<form action="https://sahabatumkm.us17.list-manage.com/subscribe/post?u=dd58ff00500b48aaaf7ad5317&amp;id=4484f1b91d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" novalidate>
 			    <div id="mc_embed_signup_scroll">
 				<h2>Subscribe to our mailing list</h2>
 			<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
@@ -40,7 +40,7 @@
 					<div class="response" id="mce-error-response" style="display:none"></div>
 					<div class="response" id="mce-success-response" style="display:none"></div>
 				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b4446f3e9bb846772656226e8_ddb0a75035" tabindex="-1" value=""></div>
+			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_dd58ff00500b48aaaf7ad5317_4484f1b91d" tabindex="-1" value=""></div>
 			    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 			    </div>
 			</form>
