8df16882cb95c3e2924c3931787f36828b4bd9ea
syarief navi <syarief.navi@gmail.com>
2018-01-16 14:01:36 +0700

    schema gallery sahabat

--- resources/views/layouts/publicbase.blade.php
+++ resources/views/layouts/publicbase.blade.php
@@ -40,6 +40,7 @@
     @if(app()->Meta->get('gtm') != '')
      <!-- Google Tag Manager --> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js? id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','{{ app()->Meta->get('gtm') }}');</script> <!-- End Google Tag Manager -->
     @endif
+    {!! PublicHelper::printSchema($var['page']) !!}
 </head>
 <body>
     @if(app()->Meta->get('gtm') != '')
